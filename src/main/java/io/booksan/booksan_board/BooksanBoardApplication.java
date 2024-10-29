883ba9fd164fa1a11c0ecb4583fd2c0f420f0e0c
MincheolSin <smc970417@gmail.com>
2024-10-29 17:18:08 +0900

    first

---
+++ src/main/java/io/booksan/booksan_board/BooksanBoardApplication.java
@@ -0,0 +1,13 @@
+package io.booksan.booksan_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksanBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksanBoardApplication.class, args);
+	}
+
+}
